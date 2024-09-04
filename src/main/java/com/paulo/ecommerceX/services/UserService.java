4cc09a18d13f9401a339bd3e525ee8c75ae41927
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-09-04 11:13:14 -0300

    feat: add UserService

---
+++ src/main/java/com/paulo/ecommerceX/services/UserService.java
@@ -0,0 +1,41 @@
+package com.paulo.ecommerceX.services;
+
+import com.paulo.ecommerceX.domain.User;
+import com.paulo.ecommerceX.domain.dto.user.UserResponseDTO;
+import com.paulo.ecommerceX.repositories.UserRepository;
+import com.paulo.ecommerceX.services.exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<UserResponseDTO> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserResponseDTO::new)
+                .toList();
+    }
+
+    public UserResponseDTO findById(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found. Id: " + id));
+        return new UserResponseDTO(user);
+    }
+
+    public void delete(UUID id){
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+        } else {
+            throw new ResourceNotFoundException("User not found. Id: " + id);
+        }
+    }
+}
