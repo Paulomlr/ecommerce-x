179ed5b600697bf5bd6ee25a2a9e2dd4db3fcce3
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-09-04 14:07:16 -0300

    feat: add createUser and updatePassword method in UserService

--- src/main/java/com/paulo/ecommerceX/services/UserService.java
+++ src/main/java/com/paulo/ecommerceX/services/UserService.java
@@ -1,13 +1,18 @@
 package com.paulo.ecommerceX.services;
 
 import com.paulo.ecommerceX.domain.User;
+import com.paulo.ecommerceX.domain.dto.user.PasswordRequestDTO;
+import com.paulo.ecommerceX.domain.dto.user.UserRequestDTO;
 import com.paulo.ecommerceX.domain.dto.user.UserResponseDTO;
+import com.paulo.ecommerceX.domain.enums.UserRole;
 import com.paulo.ecommerceX.repositories.UserRepository;
 import com.paulo.ecommerceX.services.exceptions.ResourceNotFoundException;
+import com.paulo.ecommerceX.services.exceptions.UniqueConstraintViolationException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -39,6 +44,24 @@ public class UserService implements UserDetailsService {
         return new UserResponseDTO(user);
     }
 
+    public User registerUser(UserRequestDTO body){
+        return createUser(body, UserRole.USER);
+    }
+
+    public User registerAdmin(UserRequestDTO body) {
+        return createUser(body, UserRole.ADMIN);
+    }
+
+    private User createUser(UserRequestDTO body, UserRole role) {
+        if(userRepository.findByLogin(body.login()).isPresent()) {
+            throw new UniqueConstraintViolationException("Login already exists.");
+        }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(body.password());
+        User user = new User(body.login(), encryptedPassword, role);
+
+        return userRepository.save(user);
+    }
+
     public void delete(UUID id){
         Optional<User> user = userRepository.findById(id);
         if (user.isPresent()) {
@@ -46,5 +69,19 @@ public class UserService implements UserDetailsService {
         } else {
             throw new ResourceNotFoundException("User not found. Id: " + id);
         }
+    }
+
+    public void updatePassword(String login, PasswordRequestDTO body) {
+        if (login == null) {
+            throw new RuntimeException("Invalid or expired token.");
+        }
+
+        User user = (User) userRepository.findByLogin(login)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found."));
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(body.password());
+
+        user.setPassword(encryptedPassword);
+        userRepository.save(user);
     }
 }
