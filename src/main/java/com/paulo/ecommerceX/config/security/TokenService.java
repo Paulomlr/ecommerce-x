a96cc80817cf24bf4e003aba9893daa28850700c
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-09-04 11:36:01 -0300

    feat: add TokenService and application-secrets.properties to .gitignore

---
+++ src/main/java/com/paulo/ecommerceX/config/security/TokenService.java
@@ -0,0 +1,44 @@
+package com.paulo.ecommerceX.config.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.paulo.ecommerceX.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    private final String secret;
+
+    public TokenService(@Value("${security.jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    public String generateToken(User user) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withIssuer("ecommerceSystem")
+                .withSubject(user.getLogin())
+                .withExpiresAt(generateExpirationDate())
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.require(algorithm)
+                .withIssuer("ecommerceSystem")
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
