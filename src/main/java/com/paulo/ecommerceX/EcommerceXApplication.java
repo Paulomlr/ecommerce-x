244a2999ad9e146a7fbb8614d81f4833eca11c59
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-09-04 09:01:23 -0300

    Project created

---
+++ src/main/java/com/paulo/ecommerceX/EcommerceXApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.ecommerceX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulo/ecommerceX/EcommerceXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.ecommerceX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
