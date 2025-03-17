464292cd193ad45c3b9a1fb8c1ae221e07407059
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-03-17 20:24:20 -0300

    feat: add dotenv-java support for loading .env variables

--- src/main/java/com/paulo/ecommerceX/EcommerceXApplication.java
+++ src/main/java/com/paulo/ecommerceX/EcommerceXApplication.java
@@ -1,5 +1,6 @@
 package com.paulo.ecommerceX;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,8 @@ import org.springframework.cache.annotation.EnableCaching;
 public class EcommerceXApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory(".").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(EcommerceXApplication.class, args);
 	}
 
